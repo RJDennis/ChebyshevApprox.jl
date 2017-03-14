6a8449ec624db1d1028399281a59faf6c9a58fe8
Richard Dennis <richard.dennis@glasgow.ac.uk>
2017-03-14 02:05:47 +0000

    Fixed normalize_node()

--- src/normalize_node.jl
+++ src/normalize_node.jl
@@ -1,21 +1,23 @@
 function normalize_node{T<:AbstractFloat}(node::T,domain::Array{T,1})
 
+  normalized_nodes = similar(node)
   if domain[1] == domain[2]
-    node = (domain[1]+domain[2])/2.0
+    normalized_nodes = (domain[1]+domain[2])/2.0
   else
-    node = 2.0*(node-domain[2])/(domain[1]-domain[2])-1.0
+    normalized_nodes = 2.0*(node-domain[2])/(domain[1]-domain[2])-1.0
   end
 
-  return node
+  return normalized_nodes
 
 end
 
 function normalize_node{T<:AbstractFloat}(node::Array{T,1},domain::Array{T,1})
 
+  normalized_nodes = similar(node)
   for i = 1:length(node)
-    node[i] = normalize_node(node[i],domain)
+    normalized_nodes[i] = normalize_node(node[i],domain)
   end
 
-  return node
+  return normalized_nodes
 
 end
