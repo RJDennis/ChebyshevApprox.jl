c976c3db237bb6b0dad2f0aaf20f31ae7dce3856
Richard Dennis <richard.dennis@glasgow.ac.uk>
2017-08-16 01:30:38 +0100

    Little tweeks

--- src/chebyshev_evaluate_generated.jl
+++ src/chebyshev_evaluate_generated.jl
@@ -1,6 +1,5 @@
 # Generated functions for evaluating Chebyshev polynomials
 
-#@generated function chebyshev_evaluate(weights::Array{T,N},x::Array{T,1},order::Array{S,1},domain::Array{T,2}) where {T,N,S}
 @generated function chebyshev_evaluate(weights::Array{T,N},x::Array{T,1},order::Array{S,1},domain=[ones(T,1,N);-ones(T,1,N)]) where {T,N,S}
 
   chebyshev_polynomials = :( poly = Array{T,2}[];
@@ -56,60 +55,8 @@
              )
 
   return final
-end
-
-#=
-
-@generated function chebyshev_evaluate(weights::Array{T,N},x::Array{T,1},order::Array{S,1}) where {T,N,S}
-
-  chebyshev_polynomials = :( poly = Array{T,2}[];
-                             for i = 1:size(x,1);
-                               xi = x[i];
-                               polynomial = Array{T}(1,order[i]+1);
-                               for j = 1:order[i]+1;
-                                 if j == 1;
-                                   polynomial[j] = one(T);
-                                 elseif j == 2;
-                                   polynomial[j] = xi;
-                                 else;
-                                   polynomial[j] = 2*xi*polynomial[j-1]-polynomial[j-2];
-                                 end;
-                               end;
-                               push!(poly,polynomial);
-                             end
-                             )
-
-  i_vars = Array{Symbol}(N)
-  for i = 1:N
-    i_vars[i] = Symbol("i$i")
-  end
-
-  inner_prod = :( poly[1][i1]*weights[$(i_vars...)] )
-  for i = 2:N
-    inner_prod = :( poly[$i][$(i_vars[i])]*$inner_prod )
-  end
-
-  inner = :( evaluated_polynomial += $inner_prod )
-  outer = inner
-  for i = 1:N
-    outer = :(
-    for $(i_vars[i]) = 1:size(weights,$i)
-      $outer
-    end
-    )
-  end
-
-  final = :( $chebyshev_polynomials;
-             evaluated_polynomial = zero(T);
-             $outer;
-             return evaluated_polynomial
-             )
-
-  return final
 
 end
-
-=#
 
 @generated function chebyshev_evaluate(weights::Array{T,N},x::Array{T,1},order::S,domain=[ones(T,1,N);-ones(T,1,N)]) where {T,N,S}
 
@@ -168,58 +115,5 @@ end
           )
 
   return final
+
 end
-
-#=
-
-@generated function chebyshev_evaluate(weights::Array{T,N},x::Array{T,1},order::S) where {T,N,S}
-
-  chebyshev_polynomials = :( poly = Array{T,2}[];
-                             for i = 1:size(x,1);
-                               xi = x[i];
-                               polynomial = Array{T}(1,order+1);
-                               for j = 1:order+1;
-                                 if j == 1;
-                                   polynomial[j] = one(T);
-                                 elseif j == 2;
-                                   polynomial[j] = xi;
-                                 else;
-                                   polynomial[j] = 2*xi*polynomial[j-1]-polynomial[j-2];
-                                 end;
-                               end;
-                               push!(poly,polynomial);
-                             end
-                             )
-
-  i_vars = Array{Symbol}(N)
-  for i = 1:N
-    i_vars[i] = Symbol("i$i")
-  end
-
-  inner_prod = :( poly[1][i1]*weights[$(i_vars...)] )
-  for i = 2:N
-    inner_prod = :( poly[$i][$(i_vars[i])]*$inner_prod )
-  end
-
-  inner = :( if sum(tuple($(i_vars...))) <= order+N;
-               evaluated_polynomial += $inner_prod;
-             end )
-  outer = inner
-  for i = 1:N
-    outer = :(
-      for $(i_vars[i]) = 1:size(weights,$i)
-        $outer
-      end
-    )
-  end
-
-  final = :( $chebyshev_polynomials;
-             evaluated_polynomial = zero(T);
-             $outer;
-             return evaluated_polynomial
-             )
-
-  return final
-end
-
-=#
--- src/chebyshev_weights_generated.jl
+++ src/chebyshev_weights_generated.jl
@@ -1,8 +1,6 @@
-#=
-
 # Generated functions for tensor-product polynomials where nodes are in an array of arrays
 
-@generated function chebyshev_weights(f::AbstractArray{T,N},nodes::Array{Array{T,1},1},order::Array{S,1},domain::Array{T,2}) where {T,N,S}
+@generated function chebyshev_weights(f::AbstractArray{T,N},nodes::Array{Array{T,1},1},order::Array{S,1},domain=[ones(T,1,N);-ones(T,1,N)]) where {T,N,S}
 
   chebyshev_polynomials = :( poly = Array{T,2}[];
                              for k = 1:N;
@@ -17,103 +15,6 @@
                                  xk = 2*(xk-domain[2,k])/(domain[1,k]-domain[2,k])-one(T);
                                end;
 
-                               polynomial = Array{T}(length(xk),orderk+1);
-                               for i = 1:orderk+1;
-                                 for j = 1:length(xk);
-                                   if i == 1;
-                                     polynomial[j,i] = one(T);
-                                   elseif i == 2;
-                                     polynomial[j,i] = xk[j];
-                                   else;
-                                     polynomial[j,i] = 2*xk[j]*polynomial[j,i-1]-polynomial[j,i-2];
-                                   end;
-                                 end;
-                               end;
-                               push!(poly,polynomial);
-                             end )
-
-  i_vars = Array{Symbol}(N)
-  s_vars = Array{Symbol}(N)
-  for i = 1:N
-    i_vars[i] = Symbol("i$i")
-    s_vars[i] = Symbol("s$i")
-  end
-
-  # Construct denominator term
-
-  inner_prod_denominator = :( poly[1][s1,i1] )
-  for i = 2:N
-    inner_prod_denominator = :( poly[$i][$(s_vars[i]),$(i_vars[i])]*$inner_prod_denominator )
-  end
-  denominator_term = :( $inner_prod_denominator*$inner_prod_denominator )
-
-  # Construct numerator term
-
-  inner_prod_numerator = :( poly[1][s1,i1]*f[$(s_vars...)] )
-  for i = 2:N
-      inner_prod_numerator = :( poly[$i][$(s_vars[i]),$(i_vars[i])]*$inner_prod_numerator )
-  end
-  numerator_term = inner_prod_numerator
-
-  # Construct weights term
-
-  weights_term = :( weights[$(i_vars...)] = numerator/denominator )
-
-  # Construct the inner loops that compute numerator and denominator
-
-  inner = :( numerator += $numerator_term;
-             denominator += $denominator_term )
-  outer  = inner
-
-  for i = 1:N
-    outer = :(
-      for $(s_vars[i]) = 1:size(f,$i)
-        $outer
-      end
-    )
-  end
-
-  # Construct the outer loops that compute the weights
-
-  new_inner = :( numerator = zero(T);
-                 denominator = zero(T);
-                 $outer;
-                 $weights_term )
-  new_outer = new_inner
-
-  for i = 1:N
-    new_outer = :(
-      for $(i_vars[i]) = 1:(order[$i]+1)
-        $new_outer
-      end
-    )
-  end
-
-  # Initialize the weight Array
-
-  initial_weight = string("weights = Array{T}(order[1]+1,",)
-  for i = 2:N
-    initial_weight = string(initial_weight,"order[",i,"]+1,")
-  end
-  initial_weight = parse(string(initial_weight,")"))
-
-  # Put it all together to compute the weights array
-
-  final = :( $chebyshev_polynomials;
-             $initial_weight;
-             $new_outer;
-             return weights )
-
-  return final
-
-end
-
-@generated function chebyshev_weights(f::AbstractArray{T,N},nodes::Array{Array{T,1},1},order::Array{S,1}) where {T,N,S}
-
-  chebyshev_polynomials = :( poly = Array{T,2}[];
-                             for k = 1:N;
-                               orderk = order[k];
-                               xk = nodes[k];
                                polynomial = Array{T}(length(xk),orderk+1);
                                for i = 1:orderk+1;
                                  for j = 1:length(xk);
@@ -282,11 +183,9 @@ end
 
 end
 
-=#
-
 # Generated functions for tensor-product polynomials where nodes are in a tuple
 
-@generated function chebyshev_weights(f::AbstractArray{T,N},nodes::NTuple{N,Array{T,1}},order::Array{S,1},domain::Array{T,2}) where {T,N,S}
+@generated function chebyshev_weights(f::AbstractArray{T,N},nodes::NTuple{N,Array{T,1}},order::Array{S,1},domain=[ones(T,1,N);-ones(T,1,N)]) where {T,N,S}
 
   chebyshev_polynomials = :( poly = Array{T,2}[];
                              for k = 1:N;
@@ -301,103 +200,6 @@ end
                                  xk = 2*(xk-domain[2,k])/(domain[1,k]-domain[2,k])-one(T);
                                end;
 
-                               polynomial = Array{T}(length(xk),orderk+1);
-                               for i = 1:orderk+1;
-                                 for j = 1:length(xk);
-                                   if i == 1;
-                                     polynomial[j,i] = one(T);
-                                   elseif i == 2;
-                                     polynomial[j,i] = xk[j];
-                                   else;
-                                     polynomial[j,i] = 2*xk[j]*polynomial[j,i-1]-polynomial[j,i-2];
-                                   end;
-                                 end;
-                               end;
-                               push!(poly,polynomial);
-                             end )
-
-  i_vars = Array{Symbol}(N)
-  s_vars = Array{Symbol}(N)
-  for i = 1:N
-    i_vars[i] = Symbol("i$i")
-    s_vars[i] = Symbol("s$i")
-  end
-
-  # Construct denominator term
-
-  inner_prod_denominator = :( poly[1][s1,i1] )
-  for i = 2:N
-    inner_prod_denominator = :( poly[$i][$(s_vars[i]),$(i_vars[i])]*$inner_prod_denominator )
-  end
-  denominator_term = :( $inner_prod_denominator*$inner_prod_denominator )
-
-  # Construct numerator term
-
-  inner_prod_numerator = :( poly[1][s1,i1]*f[$(s_vars...)] )
-  for i = 2:N
-      inner_prod_numerator = :( poly[$i][$(s_vars[i]),$(i_vars[i])]*$inner_prod_numerator )
-  end
-  numerator_term = inner_prod_numerator
-
-  # Construct weights term
-
-  weights_term = :( weights[$(i_vars...)] = numerator/denominator )
-
-  # Construct the inner loops that compute numerator and denominator
-
-  inner = :( numerator += $numerator_term;
-             denominator += $denominator_term )
-  outer  = inner
-
-  for i = 1:N
-    outer = :(
-      for $(s_vars[i]) = 1:size(f,$i)
-        $outer
-      end
-    )
-  end
-
-  # Construct the outer loops that compute the weights
-
-  new_inner = :( numerator = zero(T);
-                 denominator = zero(T);
-                 $outer;
-                 $weights_term )
-  new_outer = new_inner
-
-  for i = 1:N
-    new_outer = :(
-      for $(i_vars[i]) = 1:(order[$i]+1)
-        $new_outer
-      end
-    )
-  end
-
-  # Initialize the weight Array
-
-  initial_weight = string("weights = Array{T}(order[1]+1,",)
-  for i = 2:N
-    initial_weight = string(initial_weight,"order[",i,"]+1,")
-  end
-  initial_weight = parse(string(initial_weight,")"))
-
-  # Put it all together to compute the weights array
-
-  final = :( $chebyshev_polynomials;
-             $initial_weight;
-             $new_outer;
-             return weights )
-
-  return final
-
-end
-
-@generated function chebyshev_weights(f::AbstractArray{T,N},nodes::NTuple{N,Array{T,1}},order::Array{S,1}) where {T,N,S}
-
-  chebyshev_polynomials = :( poly = Array{T,2}[];
-                             for k = 1:N;
-                               orderk = order[k];
-                               xk = nodes[k];
                                polynomial = Array{T}(length(xk),orderk+1);
                                for i = 1:orderk+1;
                                  for j = 1:length(xk);
@@ -566,11 +368,9 @@ end
 
 end
 
-#=
-
 # Generated functions for complete polynomials where nodes are in an array of arrays
 
-@generated function chebyshev_weights(f::AbstractArray{T,N},nodes::Array{Array{T,1},1},order::S,domain::Array{T,2}) where {T,N,S}
+@generated function chebyshev_weights(f::AbstractArray{T,N},nodes::Array{Array{T,1},1},order::S,domain=[ones(T,1,N);-ones(T,1,N)]) where {T,N,S}
 
   chebyshev_polynomials = :( poly = Array{T,2}[];
                              for k = 1:N;
@@ -677,104 +477,6 @@ end
 
 end
 
-@generated function chebyshev_weights(f::AbstractArray{T,N},nodes::Array{Array{T,1},1},order::S) where {T,N,S}
-
-  chebyshev_polynomials = :( poly = Array{T,2}[];
-                             for k = 1:N;
-                               xk = nodes[k];
-                               polynomial = Array{T}(length(xk),order+1);
-                               for i = 1:order+1;
-                                 for j = 1:length(xk);
-                                   if i == 1;
-                                     polynomial[j,i] = one(T);
-                                   elseif i == 2;
-                                     polynomial[j,i] = xk[j];
-                                   else;
-                                     polynomial[j,i] = 2*xk[j]*polynomial[j,i-1]-polynomial[j,i-2];
-                                   end;
-                                 end;
-                               end;
-                               push!(poly,polynomial);
-                             end )
-
-  i_vars = Array{Symbol}(N)
-  s_vars = Array{Symbol}(N)
-  for i = 1:N
-    i_vars[i] = Symbol("i$i")
-    s_vars[i] = Symbol("s$i")
-  end
-
-  # Construct denominator term
-
-  inner_prod_denominator = :( poly[1][s1,i1] )
-  for i = 2:N
-    inner_prod_denominator = :( poly[$i][$(s_vars[i]),$(i_vars[i])]*$inner_prod_denominator )
-  end
-  denominator_term = :( $inner_prod_denominator*$inner_prod_denominator )
-
-  # Construct numerator term
-
-  inner_prod_numerator = :( poly[1][s1,i1]*f[$(s_vars...)] )
-  for i = 2:N
-      inner_prod_numerator = :( poly[$i][$(s_vars[i]),$(i_vars[i])]*$inner_prod_numerator )
-  end
-  numerator_term = inner_prod_numerator
-
-  # Construct weights term
-
-  weights_term = :( weights[$(i_vars...)] = numerator/denominator )
-
-  # Construct the inner loops that compute numerator and denominator
-
-  inner = :( numerator += $numerator_term;
-             denominator += $denominator_term )
-  outer  = inner
-
-  for i = 1:N
-    outer = :(
-      for $(s_vars[i]) = 1:size(f,$i)
-        $outer
-      end
-    )
-  end
-
-  # Construct the outer loops that compute the weights
-
-  new_inner = :( numerator = zero(T);
-                 denominator = zero(T);
-                 if sum(tuple($(i_vars...))) <= order+N;
-                   $outer;
-                   $weights_term;
-                 end )
-  new_outer = new_inner
-
-  for i = 1:N
-    new_outer = :(
-      for $(i_vars[i]) = 1:(order+1)
-        $new_outer
-      end
-    )
-  end
-
-  # Initialize the weight Array
-
-  initial_weight = string("weights = zeros(order+1,",)
-  for i = 2:N
-    initial_weight = string(initial_weight,"order+1,")
-  end
-  initial_weight = parse(string(initial_weight,")"))
-
-  # Put it all together to compute the weights array
-
-  final = :( $chebyshev_polynomials;
-             $initial_weight;
-             $new_outer;
-             return weights )
-
-  return final
-
-end
-
 @generated function chebyshev_weights(f::AbstractArray{T,N},poly::Array{Array{T,2},1},order::S) where {T,N,S}
 
   i_vars = Array{Symbol}(N)
@@ -854,11 +556,9 @@ end
 
 end
 
-=#
-
 # Generated functions for complete polynomials where nodes are in a tuple
 
-@generated function chebyshev_weights(f::AbstractArray{T,N},nodes::NTuple{N,Array{T,1}},order::S,domain::Array{T,2}) where {T,N,S}
+@generated function chebyshev_weights(f::AbstractArray{T,N},nodes::NTuple{N,Array{T,1}},order::S,domain=[ones(T,1,N);-ones(T,1,N)]) where {T,N,S}
 
   chebyshev_polynomials = :( poly = Array{T,2}[];
                              for k = 1:N;
@@ -872,104 +572,6 @@ end
                                  xk = 2*(xk-domain[2,k])/(domain[1,k]-domain[2,k])-one(T);
                                end;
 
-                               polynomial = Array{T}(length(xk),order+1);
-                               for i = 1:order+1;
-                                 for j = 1:length(xk);
-                                   if i == 1;
-                                     polynomial[j,i] = one(T);
-                                   elseif i == 2;
-                                     polynomial[j,i] = xk[j];
-                                   else;
-                                     polynomial[j,i] = 2*xk[j]*polynomial[j,i-1]-polynomial[j,i-2];
-                                   end;
-                                 end;
-                               end;
-                               push!(poly,polynomial);
-                             end )
-
-  i_vars = Array{Symbol}(N)
-  s_vars = Array{Symbol}(N)
-  for i = 1:N
-    i_vars[i] = Symbol("i$i")
-    s_vars[i] = Symbol("s$i")
-  end
-
-  # Construct denominator term
-
-  inner_prod_denominator = :( poly[1][s1,i1] )
-  for i = 2:N
-    inner_prod_denominator = :( poly[$i][$(s_vars[i]),$(i_vars[i])]*$inner_prod_denominator )
-  end
-  denominator_term = :( $inner_prod_denominator*$inner_prod_denominator )
-
-  # Construct numerator term
-
-  inner_prod_numerator = :( poly[1][s1,i1]*f[$(s_vars...)] )
-  for i = 2:N
-      inner_prod_numerator = :( poly[$i][$(s_vars[i]),$(i_vars[i])]*$inner_prod_numerator )
-  end
-  numerator_term = inner_prod_numerator
-
-  # Construct weights term
-
-  weights_term = :( weights[$(i_vars...)] = numerator/denominator )
-
-  # Construct the inner loops that compute numerator and denominator
-
-  inner = :( numerator += $numerator_term;
-             denominator += $denominator_term )
-  outer  = inner
-
-  for i = 1:N
-    outer = :(
-      for $(s_vars[i]) = 1:size(f,$i)
-        $outer
-      end
-    )
-  end
-
-  # Construct the outer loops that compute the weights
-
-  new_inner = :( numerator = zero(T);
-                 denominator = zero(T);
-                 if sum(tuple($(i_vars...))) <= order+N;
-                   $outer;
-                   $weights_term;
-                 end )
-  new_outer = new_inner
-
-  for i = 1:N
-    new_outer = :(
-      for $(i_vars[i]) = 1:(order+1)
-        $new_outer
-      end
-    )
-  end
-
-  # Initialize the weight Array
-
-  initial_weight = string("weights = zeros(order+1,",)
-  for i = 2:N
-    initial_weight = string(initial_weight,"order+1,")
-  end
-  initial_weight = parse(string(initial_weight,")"))
-
-  # Put it all together to compute the weights array
-
-  final = :( $chebyshev_polynomials;
-             $initial_weight;
-             $new_outer;
-             return weights )
-
-  return final
-
-end
-
-@generated function chebyshev_weights(f::AbstractArray{T,N},nodes::NTuple{N,Array{T,1}},order::S) where {T,N,S}
-
-  chebyshev_polynomials = :( poly = Array{T,2}[];
-                             for k = 1:N;
-                               xk = nodes[k];
                                polynomial = Array{T}(length(xk),order+1);
                                for i = 1:order+1;
                                  for j = 1:length(xk);
