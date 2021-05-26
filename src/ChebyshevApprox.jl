dde2b0b5313d27f73f99758909afdf3974c97fe9
RJDennis <richard.dennis@glasgow.ac.uk>
2021-05-26 10:58:14 +0100

    Update ChebyshevApprox.jl

--- src/ChebyshevApprox.jl
+++ src/ChebyshevApprox.jl
@@ -7,6 +7,7 @@ include("structures.jl")
 include("chebyshev_nodes.jl")
 include("chebyshev_extrema.jl")
 include("chebyshev_extended.jl")
+include("chebyshev_vertesi.jl")
 include("normalize_node.jl")
 include("chebyshev_polynomial.jl")
 include("chebyshev_weights.jl")
@@ -17,18 +18,22 @@ include("clenshaw_evaluate_generated.jl")
 export ChebPoly,
        ChebInterpRoots,
        ChebInterpExtrema,
-       ChebInterpExtended
+       ChebInterpExtended,
+       ChebInterpVertesi
 
 export chebyshev_nodes,
        chebyshev_extrema,
        chebyshev_extended,
+       chebyshev_vertesi,
        chebyshev_polynomial,
        chebyshev_weights,
        chebyshev_weights_extrema,
        chebyshev_weights_extended,
+       chebyshev_weights_vertesi,
        chebyshev_weights_threaded,
        chebyshev_weights_extrema_threaded,
        chebyshev_weights_extended_threaded,
+       chebyshev_weights_vertesi_threaded,
        chebyshev_evaluate,
        cheb_interp,
        chebyshev_derivative,
