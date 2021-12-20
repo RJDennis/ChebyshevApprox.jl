f3052ad7b6eab8bcec2930b111b52f92d5b00586
RJDennis <richard.dennis@glasgow.ac.uk>
2021-12-20 18:26:52 +0000

    Generalize function-types

--- src/normalize_node.jl
+++ src/normalize_node.jl
@@ -1,4 +1,4 @@
-function normalize_node(node::T,domain::Array{T,1}) where {T<:AbstractFloat}
+function normalize_node(node::R,domain::Array{T,1}) where {R<:Number,T<:AbstractFloat}
 
   if domain[1] == domain[2]
     norm_node = zero(T)
@@ -7,24 +7,24 @@ function normalize_node(node::T,domain::Array{T,1}) where {T<:AbstractFloat}
     norm_node = 2.0*(node-domain[2])/(domain[1]-domain[2])-1.0
     return norm_node
   end
-
+  
 end
-
-function normalize_node(node::Array{T,1},domain::Array{T,1}) where {T<:AbstractFloat}
-
+  
+function normalize_node(node::AbstractArray{R,1},domain::Array{T,1}) where {R<:Number,T<:AbstractFloat}
+  
   norm_nodes = similar(node)
   for i in eachindex(node)
     norm_nodes[i] = normalize_node(node[i],domain)
   end
-
+  
   return norm_nodes
-
+  
 end
-
-function normalize_node!(node::Array{T,1},domain::Array{T,1}) where {T<:AbstractFloat}
-
+  
+function normalize_node!(node::AbstractArray{R,1},domain::Array{T,1}) where {R<:Number,T<:AbstractFloat}
+  
   for i in eachindex(node)
     node[i] = normalize_node(node[i],domain)
   end
-
+  
 end
